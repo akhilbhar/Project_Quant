64da87cab5317f1ac1976aa80a99e74f62d881cf
Akhil Bhargava <akhil.bhargav@gmail.com>
2016-12-29 22:01:04 -0600

    on 29/12/2016

--- code/CODE1.R
+++ code/CODE1.R
@@ -1,12 +1,14 @@
 ####Installing Packages####----------
 #install.packages("foreign")
-library('foreign')
-
+library(foreign)
+library(help=foreign)
+install.packages("rio")
 
 ###Save and Load####
 
 Project_Dir ="C:/Users/akhil/Desktop/R/Project_Quant"
 RData=paste(Project_Dir,"/","Project_Quant.RData",sep="")
+
 save.image(file=RData)
 
 load(file=RData)
@@ -25,6 +27,31 @@ colnames(Data)=Col_Names[,1]
 
 
 #Raw_Data=read.dta(file, convert.dates = TRUE, convert.factors = TRUE,missing.type = FALSE,convert.underscore = FALSE, warn.missing.labels = TRUE)
+
+###############################
+#Data Chapter 4 Quant
+###############################
+library(foreign)
+dir="/Users/akhil/Desktop/R/Quant/Classroom_Labs/Chapter4"
+file_name="Data_Ch4.dta"
+file=paste(dir,"/",file_name,sep="")
+Data_Ch4=read.dta(file)
+
+###############################
+# Calculating Price Return
+###############################
+Data_Ch4_Price=Data_Ch4[,1:4]
+Ticker_list=unique(Data_Ch4_Price[,1])
+for(j in 1:length(Ticker_list)){
+Ticker= Ticker_list[j]
+L=length(Data_Ch4_Price[Data_Ch4_Price[,1]==Ticker,3])
+Data_Ch4_Price[Data_Ch4_Price[,1]==Ticker,"priceRt3"] =NA
+if(L>3){ 
+  for(i in 4:L ){
+    Data_Ch4_Price[Data_Ch4_Price[,1]==Ticker,"priceRt3"][i]=(Data_Ch4_Price[Data_Ch4_Price[,1]==Ticker,4][i]-Data_Ch4_Price[Data_Ch4_Price[,1]==Ticker,4][i-3])/Data_Ch4_Price[Data_Ch4_Price[,1]==Ticker,4][i-3]
+  } }}
+
+
 
 
 
